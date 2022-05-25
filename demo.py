c381efd17b4d346b5ebda486560133e82bdc3e77
CodingChan <codingchan@gmail.com>
2022-05-26 00:18:38 +0800

    choropleth show lastest info
    Signed-off-by: CodingChan <codingchan@gmail.com>

--- demo.py
+++ demo.py
@@ -4,27 +4,41 @@ import pandas as pd
 import io
 import requests
 
-my_url = 'https://raw.githubusercontent.com/globaldothealth/monkeypox/main/latest.csv'
-proxy_dict = {
-    'https': 'http://127.0.0.1:10809'
-}
-s = requests.get(my_url, proxies=proxy_dict).text
-csv = pd.read_csv(io.StringIO(s))
-counted = csv.groupby(['Country']).size()
-counted.to_csv("./counted.csv")
-df = pd.read_csv('./counted.csv')
+import plotly.express as px
+import matplotlib.pyplot as plt
+
+# my_url = 'https://raw.githubusercontent.com/globaldothealth/monkeypox/main/latest.csv'
+# proxy_dict = {
+#     'https': 'http://127.0.0.1:10809'
+# }
+# s = requests.get(my_url, proxies=proxy_dict).text
+# csv = pd.read_csv(io.StringIO(s))
+# counted = csv.groupby(['Country']).size()
+# counted.to_csv("./counted.csv")
+df_num = pd.read_csv('./counted.csv')
+
+# import seaborn as sns
+# sns.set(style="darkgrid")
+# sns.histplot(data=df, x="0")
+# plt.show();
 
 df = px.data.gapminder()
-fig = px.choropleth(df, locations="iso_alpha", color="lifeExp", hover_name="country", animation_frame="year", range_color=[20,80])
+df_merged = pd.merge(df, df_num, on='country')
+
+fig = px.choropleth(df_merged,
+                    locations="iso_alpha",
+                    color="num",
+                    hover_name="country")
 fig.show()
 
-import plotly.express as px
+# fig = px.choropleth(df, locations='', color='unemp',
+#                            color_continuous_scale="Viridis",
+#                            range_color=(0, 12),
+#                            scope="usa",
+#                            labels={'unemp':'unemployment rate'}
+#                           )
+# fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+# fig.show()
 
-fig = px.choropleth(df, geojson=counties, locations='', color='unemp',
-                           color_continuous_scale="Viridis",
-                           range_color=(0, 12),
-                           scope="usa",
-                           labels={'unemp':'unemployment rate'}
-                          )
-fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+
 fig.show()
