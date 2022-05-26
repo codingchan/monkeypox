a063b98c782d67797994e889ff83a29aabffec08
codingchan <codingchan@gmail.com>
2022-05-26 16:11:03 +0800

    update demo
    Signed-off-by: codingchan <codingchan@gmail.com>

--- demo.py
+++ demo.py
@@ -1,30 +1,24 @@
 import plotly.graph_objects as go
-
+import plotly.express as px
 import pandas as pd
 import io
 import requests
 
-my_url = 'https://raw.githubusercontent.com/globaldothealth/monkeypox/main/latest.csv'
-proxy_dict = {
-    'https': 'http://127.0.0.1:10809'
-}
-s = requests.get(my_url, proxies=proxy_dict).text
-csv = pd.read_csv(io.StringIO(s))
-counted = csv.groupby(['Country']).size()
-counted.to_csv("./counted.csv")
-df = pd.read_csv('./counted.csv')
-
-df = px.data.gapminder()
-fig = px.choropleth(df, locations="iso_alpha", color="lifeExp", hover_name="country", animation_frame="year", range_color=[20,80])
-fig.show()
-
-import plotly.express as px
-
-fig = px.choropleth(df, geojson=counties, locations='', color='unemp',
-                           color_continuous_scale="Viridis",
-                           range_color=(0, 12),
-                           scope="usa",
-                           labels={'unemp':'unemployment rate'}
-                          )
+# my_url = 'https://raw.githubusercontent.com/globaldothealth/monkeypox/main/latest.csv'
+# proxy_dict = {
+#     'https': 'http://127.0.0.1:10809'
+# }
+# s = requests.get(my_url, proxies=proxy_dict).text
+# csv = pd.read_csv(io.StringIO(s))
+# counted = csv.groupby(['Country']).size()
+# counted.to_csv("./counted.csv")
+df_counted = pd.read_csv('./counted.csv')
+df = px.data.gapminder().query("year==2007")
+df_merged = pd.merge(df, df_counted, on='country')
+fig = px.choropleth(df_merged,
+                    locations="iso_alpha",
+                    color="num",
+                    hover_name="country",
+                    hover_data=['num'])
 fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
 fig.show()
