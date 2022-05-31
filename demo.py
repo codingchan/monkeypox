32684169fc01fc1bf6efe77e881f33c2483d9c5e
codingchan <codingchan@gmail.com>
2022-05-31 18:16:10 +0800

    updated demo
    Signed-off-by: codingchan <codingchan@gmail.com>

--- demo.py
+++ demo.py
@@ -4,28 +4,33 @@ import pandas as pd
 import io
 import requests
 
-# my_url = 'https://raw.githubusercontent.com/globaldothealth/monkeypox/main/latest.csv'
-# proxy_dict = {
-#     'https': 'http://127.0.0.1:10809'
-# }
-# s = requests.get(my_url, proxies=proxy_dict).text
-# csv = pd.read_csv(io.StringIO(s))
-# counted = csv.groupby(['Country']).size()
-# counted.to_csv("./counted.csv")
+# 获取最新的疫情数据
+my_url = 'https://raw.githubusercontent.com/globaldothealth/monkeypox/main/latest.csv'
+proxy_dict = {
+    'https': 'http://127.0.0.1:10809'
+}
+s = requests.get(my_url, proxies=proxy_dict).text
+df = pd.read_csv(io.StringIO(s))
 
-df_counted = pd.read_csv('./counted.csv')
-df = px.data.gapminder().query("year==2007")
-df_merged = pd.merge(df, df_counted, on='country')
+# 处理数据
+# df.rename(columns={'Country': 'country'}, inplace=True)
+df_grouped = df.groupby('Country_ISO3').size().reset_index(name='num')
+df_grouped.rename(columns={'Country_ISO3': 'iso_alpha', '0': 'num'}, inplace=True)
+
+# 获取地理数据，合并数据
+df_geo = px.data.gapminder().query("year==2007")
+df_merged = pd.merge(df_geo, df_grouped, on='iso_alpha')
+
+# choropleth展示
 fig = px.choropleth(df_merged,
                     locations="iso_alpha",
                     color="num",
                     hover_name="country",
                     hover_data=['num'])
-# fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
 
 import dash
-import dash_core_components as dcc
-import dash_html_components as html
+from dash import dcc
+from dash import html
 
 app = dash.Dash()
 app.layout = html.Div([
