6eea9fed402251a008297b9c64d1355e559711b8
CodingChan <codingchan@gmail.com>
2022-06-03 02:35:48 +0800

    refresh page when reload
    Signed-off-by: CodingChan <codingchan@gmail.com>

--- main.py
+++ main.py
@@ -4,44 +4,53 @@ import pandas as pd
 import io
 import requests
 
-# 获取最新的疫情数据
-my_url = 'https://raw.githubusercontent.com/globaldothealth/monkeypox/main/latest.csv'
-proxy_dict = {
-    'https': 'http://127.0.0.1:10809'
-}
-s = requests.get(my_url, proxies=proxy_dict).text
-df = pd.read_csv(io.StringIO(s))
-
-# 处理数据
-# df.rename(columns={'Country': 'country'}, inplace=True)
-df_grouped = df.groupby(['Country_ISO3', 'Country']).size().reset_index(name='Confirmed & Suspected')
-df_grouped.rename(columns={'Country_ISO3': 'iso_alpha', '0': 'Confirmed & Suspected'}, inplace=True)
-
-# 获取地理数据，合并数据
-df_geo = px.data.gapminder().query("year==2007")
-df_merged = pd.merge(df_geo, df_grouped, on='iso_alpha')
-df_merged.sort_values(by=['Confirmed & Suspected'], ascending=False, inplace=True)
-
-# choropleth展示
-fig = px.choropleth(df_merged,
-                    locations="iso_alpha",
-                    color="Confirmed & Suspected",
-                    hover_name="country",
-                    hover_data=['Confirmed & Suspected'])
-
-fig_bubble = px.scatter_geo(df_merged,
-                            locations="iso_alpha",
-                            color="Confirmed & Suspected",
-                            hover_name="country",
-                            size="Confirmed & Suspected",
-                            # projection="natural earth"
-                            )
-
 import dash
 from dash import dcc
 from dash import html
+from dash.dependencies import Input, Output
 
 app = dash.Dash()
+
+
+def fetch_data():
+    # 获取最新的疫情数据
+    my_url = 'https://raw.githubusercontent.com/globaldothealth/monkeypox/main/latest.csv'
+    proxy_dict = {
+        'https': 'http://127.0.0.1:10809'
+    }
+    s = requests.get(my_url, proxies=proxy_dict).text
+    df = pd.read_csv(io.StringIO(s))
+
+    # 处理数据
+    # df.rename(columns={'Country': 'country'}, inplace=True)
+    df_grouped = df.groupby(['Country_ISO3', 'Country']).size().reset_index(name='Confirmed & Suspected')
+    df_grouped.rename(columns={'Country_ISO3': 'iso_alpha', '0': 'Confirmed & Suspected'}, inplace=True)
+
+    # 获取地理数据，合并数据
+    df_geo = px.data.gapminder().query("year==2007")
+    df_merged = pd.merge(df_geo, df_grouped, on='iso_alpha')
+    df_merged.sort_values(by=['Confirmed & Suspected'], ascending=False, inplace=True)
+
+    return df_merged
+
+
+# choropleth展示
+def generate_fig():
+    return px.choropleth(fetch_data(),
+                         locations="iso_alpha",
+                         color="Confirmed & Suspected",
+                         hover_name="country",
+                         hover_data=['Confirmed & Suspected'])
+
+
+def generate_fig_bubble():
+    return px.scatter_geo(fetch_data(),
+                          locations="iso_alpha",
+                          color="Confirmed & Suspected",
+                          hover_name="country",
+                          size="Confirmed & Suspected",
+                          # projection="natural earth"
+                          )
 
 
 def generate_table(df_grouped, max_rows=200):
@@ -59,42 +68,47 @@ def generate_table(df_grouped, max_rows=200):
     ])
 
 
-colors = {
-    'background': '#FFF',
-    'text': '#7FDBFF'
-}
-app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
-    html.H1(
-        children='全球猴痘疫情实时数据',
-        style={
-            'textAlign': 'center',
-            'color': colors['text']
-        }
-    ),
-    # html.Div(children='全球猴痘疫情地图', style={
-    #     'textAlign': 'center',
-    #     'color': colors['text']
-    # }),
-    html.Div(children=[
-        html.Div(
-            style={'width': '65%', 'float': 'left'},
-            children=[
-                html.Div(
-                    children=[
-                        dcc.Graph(figure=fig),
-                        dcc.Graph(figure=fig_bubble)
-                    ]
-                )
-            ]
+def serve_layout():
+    colors = {
+        'background': '#FFF',
+        'text': '#7FDBFF'
+    }
+    layout = html.Div(style={'backgroundColor': colors['background']}, children=[
+        html.H1(
+            children='全球猴痘疫情实时数据',
+            style={
+                'textAlign': 'center',
+                'color': colors['text']
+            }
         ),
-        html.Div(
-            style={'width': '30%', 'float': 'right'},
-            children=[
-                html.H4(children='各国猴痘疫情数据'),
-                generate_table(df_merged)
-            ]
-        )
+        # html.Div(children='全球猴痘疫情地图', style={
+        #     'textAlign': 'center',
+        #     'color': colors['text']
+        # }),
+        html.Div(children=[
+            html.Div(
+                style={'width': '65%', 'float': 'left'},
+                children=[
+                    html.Div(
+                        children=[
+                            dcc.Graph(figure=generate_fig()),
+                            dcc.Graph(figure=generate_fig_bubble()),
+                        ]
+                    )
+                ]
+            ),
+            html.Div(
+                style={'width': '30%', 'float': 'right'},
+                children=[
+                    html.H4(children='各国猴痘疫情数据'),
+                    generate_table(fetch_data())
+                ]
+            )
+        ])
     ])
-])
 
+    return layout
+
+
+app.layout = serve_layout()
 app.run_server(debug=True, use_reloader=False)  # Turn off reloader if inside Jupyter
